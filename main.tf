a6ccb4d7bebf60d366839da6b7e3fda89a956932
Miguel <miguel.ramirezr@upb.edu.co>
2025-10-29 17:18:02 +0000

    Commit adding to main the complete content for frontend

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.small"
 }
 
 variable "key_name" {
